3cb30c13a1193ba1223aeee856854b631485700d
NL647 <76846739+NL647@users.noreply.github.com>
2024-08-31 11:37:00 +0700

    Update audit.sh

--- audit.sh
+++ audit.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-# Define output file
-OUTPUT_FILE="/tmp/{{ csv_name }}"
+# Function to display usage
+usage() {
+    printf "Usage: %s -f output_filename.csv\n" "$(basename "$0")"
+    exit 1
+}
+
+# Parse command line arguments
+while getopts "f:" opt; do
+    case "$opt" in
+        f) OUTPUT_FILE="$OPTARG" ;;
+        *) usage ;;
+    esac
+done
+
+# Ensure the output file is specified
+if [[ -z "$OUTPUT_FILE" ]]; then
+    printf "Error: Output file name is required.\n" >&2
+    usage
+fi
 
 # Collect information
 HOSTNAME=$(hostname)
